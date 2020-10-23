382f7270093f38a0f24ae617c15fb6e285d189a6
Yaaz <yagizcemc@gmail.com>
2020-10-23 19:53:13 +0300

    Update Kitaplik.h

--- Kitaplik.h
+++ Kitaplik.h
@@ -1,10 +1,5 @@
-// Yaðýzcem CENGÝZ
-// 170757058
-// Bilgisayar Mühendisliði
-// Nesneye Yönelik Programlama BIM - 104
-// II. Dönem (2. yarýyýl)
-// Proje3
-// Dr. Öðr. Üyesi Gülþah TÜMÜKLÜ ÖZYER
+// YaÄŸÄ±zcem CENGÄ°Z
+
 
 
 #include "Kitap.h"        // Kitap classini dahil ettik
@@ -18,11 +13,11 @@ class Kitaplik
 		int kitapSayisi = 0;				// Kitap sayisini en basta 0'a esitliyoruz.
 		void kitapEkle(Kitap kitap);		// 
 		int kacKitap();						//
-		void kitaplariBastir();				// Ýstenilen fonksiyonlari yazdik.
+		void kitaplariBastir();				// Ä°stenilen fonksiyonlari yazdik.
 		void kullaniciGirdisi();			//
 
 	private:		
-		Kitap kitaplar[10] = {};     // içinde max 10 adet kitap bulunduracak kitaplar adli Kitap nesnesi olusturduk.
+		Kitap kitaplar[10] = {};     // iÃ§inde max 10 adet kitap bulunduracak kitaplar adli Kitap nesnesi olusturduk.
 
 
 };
